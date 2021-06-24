1e3256a1ab6a524dfb77e3fc8a35cefea934e0bb
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2021-06-23 20:52:53 -0700

    Add endpoint to GET questions

--- collab/src/main.rs
+++ collab/src/main.rs
@@ -1,8 +1,8 @@
-#![allow(dead_code)]
+use serde::Serialize;
 use std::{io::Error, io::ErrorKind, str::FromStr};
 use warp::Filter;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
 impl FromStr for QuestionId {
@@ -16,7 +16,7 @@ impl FromStr for QuestionId {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -35,8 +35,24 @@ impl Question {
     }
 }
 
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+
+    Ok(warp::reply::json(&question))
+}
+
 #[tokio::main]
 async fn main() {
-    let hello = warp::get().map(|| format!("Hello, World!"));
-    warp::serve(hello).run(([127, 0, 0, 1], 1337)).await;
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
+
+    let routes = get_items;
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
